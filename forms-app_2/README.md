5da651bc32fd115310770e0f14460ce76a2b0ebf
kweku-annan <kwekuannan.dev@gmail.com>
2022-04-16 21:06:55 +0000

    Added logic to the UI and also added a README file.

---
+++ forms-app_2/README.md
@@ -0,0 +1,10 @@
+# Data Collector.
+
+This is a small utility desktop app created with Python Kivy. 
+
+I initially did this with Python's tkinter module. And now, I have used kivy to modify it.
+
+The app is used to collect some basic data about attendees of an event. It then stores the data
+collected in a csv file, under the columns: First name, Surname, Contact, Email and Location.
+
+This is not a finished project. Updates will come soon.
--- forms-app_2/main.py
+++ forms-app_2/main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang.builder import Builder
@@ -15,12 +16,57 @@ Builder.load_file('main.kv')
 class MainWindow(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        pass
+
+    def clear(self):
+        self.ids.participant_name.text = ""
+        self.ids.contact.text = ""
+        self.ids.email.text = ""
+        self.ids.location.text = ""
+
+    def save_data(self):
+        """The purpose of this function is to collect all the entries in the TextInput and save it in a CSV and xlsx
+        files under their respective headers.
+        """
+        name = self.ids.participant_name.text
+        contact = self.ids.contact.text
+        email = self.ids.email.text
+        location = self.ids.location.text
+
+        # print(f"{name}'s contact is {contact}, email is {email} and stays at {location}")
+        first_name = name.split(" ")[0]
+        surname = ""
+        if len(name.split(" ")) - 1 > 0:
+            surname = name.split(" ")[1]
+
+        # Creating the file and its entries.
+
+        file_name = "test_file"
+        file_path = f"forms_data/{file_name}"  # Setting the file name
+
+        data_dict = {
+            "First Name": [first_name.title()],
+            "Surname": [surname.title()],
+            "Contact": [contact],
+            "Email": [email],
+            "Location": [location.title()],
+        }
+        data = pd.DataFrame(data_dict, index=None)
+        # print(data.head())
+
+        with open(file_path, "a") as file:
+            data.to_csv(f"{file}.csv", mode='a', header=file.tell() == 0)
+
+        self.clear()
+        # if os.path.isfile(f"{file_path}.xlsx") is False or os.path.isfile(f"{file_name}.csv") is False:
+        # data.to_excel(f"{file_path}.xlsx") data.to_csv(f"{file_path}.csv") elif os.path.isfile(f"{file_path}.xlsx")
+        # is True or os.path.isfile(f"{file_path}.csv") is True: data.to_csv(f"{file_path}.csv", mode='a',
+        # header=False) with pd.ExcelWriter(path=f"{file_path}", engine="openpyxl", mode="a",
+        # if_sheet_exists="overlay") as writer: data.to_excel(writer, sheet_name=f"{file_name}")
 
 
 class DataCollectionApp(App):
     def build(self):
-        # Window.title = "Data Collection App"
+        self.title = "Data Collector"
         return MainWindow()
 
 
